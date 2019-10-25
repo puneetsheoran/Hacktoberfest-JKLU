44ccad7340fa352c8df4cbe2ef900721b3a3c5cd
puneetsheoran <56474921+puneetsheoran@users.noreply.github.com>
2019-10-25 13:13:33 +0530

    Update bhanu.py

--- bhanu.py
+++ bhanu.py
@@ -1,4 +1,5 @@
-name="Bhanu"
+name="Bhanu soni"
 year="First"
 branch="Computer Science"
+university="Jk Lxmipath University"
 print("Hello! I am "+name+" from "+year+" year "+branch+" and today's Hactoberfest event was fun!!!!  ")
